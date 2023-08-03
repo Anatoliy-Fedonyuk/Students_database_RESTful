dae9dd4137cc25934bfa0c17e5d0822a4f8b6af2
Anatoliy 1974 <126827447+Anatoliy-Fedonyuk@users.noreply.github.com>
2023-08-03 21:42:36 +0200

    vers 0.1

--- app/models.py
+++ app/models.py
@@ -1,29 +1,64 @@
 import psycopg2
 from config import host, user, password, db_name
 
-try:
-    connection = psycopg2.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=db_name
-    )
-    connection.autocommit = True
 
-    with connection.cursor() as cursor:
-        cursor.execute(f"GRANT ALL PRIVILEGES ON DATABASE {db_name} TO {user};")
-        print(f"[INFO] User '{user}' rights set for the database '{db_name}'")
-
-        cursor.execute("SELECT version();")
-        print(f'Server version: {cursor.fetchone()}')
-
-        cursor.execute("SELECT * FROM users;")
-        [print(res) for res in cursor]
+def grant_user_privileges(conn, db, superuser):
+    with conn.cursor() as cursor:
+        cursor.execute(f"GRANT ALL PRIVILEGES ON DATABASE {db} TO {superuser};")
+        print(f"[INFO] User '{superuser}' rights set for the database '{db}'")
 
 
-except Exception as _ex:
-    if connection: connection.rollback()
-    print("[INFO] Error while working with PostgreSQL", _ex)
-finally:
-    if connection: connection.close()
-    print("[INFO] PostgreSQL connection closed")
+def create_tables(conn):
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute("""CREATE TABLE IF NOT EXISTS groups
+                            (group_id serial PRIMARY KEY,
+                            name varchar(50) NOT NULL UNIQUE);""")
+            print("[INFO] Table 'groups' created successfully")
+
+            cursor.execute("""CREATE TABLE IF NOT EXISTS students
+                            (id serial PRIMARY KEY,
+                            first_name varchar(50) NOT NULL UNIQUE,
+                            last_name varchar(50) NOT NULL UNIQUE,
+                            age INTEGER NOT NULL,
+                            group_id INTEGER REFERENCES groups(group_id));""")
+            print("[INFO] Table 'students' created successfully")
+
+            cursor.execute("""CREATE TABLE IF NOT EXISTS courses
+                            (id_course serial PRIMARY KEY,
+                            course varchar(50) NOT NULL UNIQUE,
+                            description varchar(150) NOT NULL);""")
+            print("[INFO] Table 'courses' created successfully")
+
+        except Exception as ex:
+            print("[ERROR] Error while creating tables:", ex)
+
+
+def main():
+    connection = None
+
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name
+        )
+        connection.autocommit = True
+
+        grant_user_privileges(connection, db_name, user)
+        print("[INFO] PostgreSQL connection opened")
+
+        create_tables(connection)
+
+    except Exception as ex:
+        print("[ERROR] Error while working with PostgreSQL:", ex)
+
+    finally:
+        if connection is not None:
+            connection.close()
+            print("[INFO] PostgreSQL connection closed")
+
+
+if __name__ == "__main__":
+    main()
