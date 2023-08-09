cf99d3d73e31f08f7444038ca3bf6f5c46d9f74a
Anatoliy 1974 <126827447+Anatoliy-Fedonyuk@users.noreply.github.com>
2023-08-09 20:32:56 +0200

    vers 0.2

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from sqlalchemy import inspect
 from models import db, main_models
+from flask_migrate import Migrate
 
 from config_db import host, user, password, db_name, main_config
 from generator import generate_groups, generate_students, generate_courses, generate_student_course
@@ -9,6 +10,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}/{db_name}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+migrate = Migrate(app, db)
 
 
 def check_tables():
