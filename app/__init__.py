f48140e68069fe4ccadd543d724143c80102b367
Anatoliy 1974 <126827447+Anatoliy-Fedonyuk@users.noreply.github.com>
2023-08-12 13:15:58 +0200

    vers 0.2

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,28 @@
-import os
-import sys
+from flask import Flask
+from config_db import host, user, password, db_name
+from models import db, main_models
+from flask_migrate import Migrate
+from views import groups_bp, students_bp, courses_bp, student_course_bp
 
-sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+def create_app(config_name):
+    app = Flask(__name__)
+
+    if config_name == 'production':
+        app.config.from_object('config.production')
+    elif config_name == 'testing':
+        app.config.from_object('config.testing')
+    else:
+        app.config.from_object('config.development')
+
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    with app.app_context():
+        main_models()
+
+    app.register_blueprint(groups_bp)
+    app.register_blueprint(students_bp)
+    app.register_blueprint(courses_bp)
+    app.register_blueprint(student_course_bp)
+
+    return app
