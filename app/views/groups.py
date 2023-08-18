8cca0cff141a54e67d38c1921aae307439f7bfec
Anatoliy 1974 <126827447+Anatoliy-Fedonyuk@users.noreply.github.com>
2023-08-18 13:06:43 +0200

    vers 0.4

--- app/views/groups.py
+++ app/views/groups.py
@@ -7,12 +7,26 @@ from app.generator import db, Groups, Students
 
 class AllGroupsResource(Resource):
     def get(self):
+        groups = (db.session.query(Groups.group_id, Groups.name, func.count(Students.id).label('student_count'))
+                  .outerjoin(Students, Groups.group_id == Students.group_id)
+                  .group_by(Groups.group_id).all())
+
+        groups.sort(key=Groups.group_id)
+
+        result = [{'id': id, 'group_name': name, 'student_count': count} for id, name, count in groups]
+
+        return jsonify(result)
+
+class GroupsOnRequestResource(Resource):
+    def get(self, num):
         groups = (db.session.query(Groups.name, func.count(Students.id).label('student_count'))
                   .outerjoin(Students, Groups.group_id == Students.group_id)
-                  .group_by(Groups.name).all())
+                  .group_by(Groups.name)
+                  .having(func.count(Students.id) >= num)
+                  .all())
 
         groups.sort(key=lambda group: group.student_count)
 
-        result = [{'group_name': group_name, 'student_count': student_count} for group_name, student_count in groups]
+        result = [{'group_name': name, 'student_count': count} for name, count in groups]
 
         return jsonify(result)
